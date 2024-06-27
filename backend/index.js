7e654855ec5b3dca8222f2cd29d167cbc7cf589e
albertceballos0 <albertceballos37@gmail.com>
2024-06-27 21:58:32 +0200

    chore: Elimina componente CameraApp y archivos relacionados

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,7 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes.js');
-const fileRecognitionRoutes = require('./routes/fileRecognition.js');
 const graphRoutes = require('./routes/graphRoutes.js');
-const cameraAppRoutes = require('./routes/cameraAppRoutes.js');
-const socketIo = require('socket.io');
-const multer = require('multer');
-const { use } = require('bcrypt/promises.js');
-const upload = multer({ dest: 'uploads/' });
-
 
 require('dotenv').config();
 
@@ -25,89 +18,8 @@ app.use(cors());
 // Ruta base para usuarios
 app.use('/users', userRoutes);
 app.use('/graph', graphRoutes);
-app.use('/camera', cameraAppRoutes);
-app.use('/filerecognition', fileRecognitionRoutes);
-
 
 const server = http.createServer(app);
-const io = socketIo(server);
-
-
-
-let confirm = null;
-let msg = null;
-let username = null;
-
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-    
-  socket.on('message', (message) =>{
-    if(msg === message) return;
-    else{
-      console.log(message);
-      msg = message;
-      socket.broadcast.emit('message' , message);
-    }
-  }); 
-  socket.on('prepared', (data) =>{
-    console.log('prepared', data);
-    socket.broadcast.emit('prepared', data);
-    
-  });
-  socket.on('conectado', (data) =>{
-    console.log('conectado', data);
-    socket.broadcast.emit('conectado', data);
-    
-  });
-  socket.on('withoutTrack', (message) =>{
-    if(msg === message) return -1;
-    else{
-      msg = message;
-      console.log(message);
-      socket.broadcast.emit('withoutTrack' , message);
-    }
-  });
-  socket.on('onTrack', (message) =>{
-    if(msg === message) return -1;
-    else{
-      msg = message;
-      console.log(message);
-      socket.broadcast.emit('onTrack' , message);
-
-    }
-  });
-  socket.on('logueado', (message) =>{
-      console.log('logueado', message);
-      socket.broadcast.emit('logueado' , message);
-    
-  });
-  socket.on('logout', (message) =>{
-      console.log(message);
-      socket.broadcast.emit('logout' , message);
-    
-  });
-  
-
-  socket.on('confirm', (peerId) => {
-    if(confirm === peerId) return;
-    else confirm = peerId;
-    // Broadcast the peerId to all connected clients except the sender
-    console.log(peerId);
-    socket.broadcast.emit('confirm', peerId);
-  });
-
-  socket.on('enviar', () => {
-    socket.broadcast.emit('enviar');
-  });
-
-
-});
-
-
 
 const PORT = process.env.PORT;
 server.listen(PORT, () => {
